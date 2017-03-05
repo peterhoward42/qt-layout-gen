bea63bd5c41b0305d502b6d2793b2695b63fd5cf
peterhoward42 <peterhoward42@gmail.com>
2017-03-05 12:20:10 +0000

    Trying to get PyCharm to recognize GitHub integration.

---
+++ src/qtlayoutbuilder/builderror_test.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+
+class TestBuildError(TestCase):
+    def test_push_message(self):
+        self.fail()
+
+    def test_format_as_single_string(self):
+        self.pass()
