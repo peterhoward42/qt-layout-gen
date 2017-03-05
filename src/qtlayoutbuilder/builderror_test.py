4146be648f04ed409eb82e43528bc700751ef03c
peterhoward42 <peterhoward42@gmail.com>
2017-03-05 12:48:51 +0000

    Put in first proper tiny package class and unit test.

--- src/qtlayoutbuilder/builderror_test.py
+++ src/qtlayoutbuilder/builderror_test.py
@@ -1,9 +1,13 @@
 from unittest import TestCase
 
+from builderror import BuildError
 
 class TestBuildError(TestCase):
-    def test_push_message(self):
-        self.fail()
 
-    def test_format_as_single_string(self):
-        self.faildoo()
+    def test_that_multiple_pushed_messages_are_formatted_properly_when_asked_for(self):
+        err = BuildError()
+        err.push_message('message about error details')
+        err.push_message('message about error context')
+        formatted_message = err.format_as_single_string()
+        self.assertEquals(formatted_message, 'message about error context\nmessage about error details')
+
