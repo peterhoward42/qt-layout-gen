81b8c46c96afe13940b588130db90978ab63e07e
Peter Howard <peter.howard@diasemi.com>
2017-03-16 16:44:58 +0000

    Finished objectfinder.

--- src/qtlayoutbuilder/objectfinder.py
+++ src/qtlayoutbuilder/objectfinder.py
@@ -1,36 +1,27 @@
 import gc
 
+
 class ObjectFinder(object):
     """
-    This class helps you find an object that is already instantiated in your
-    program - by searching for the name you assigned it to when you
-    instantiated it.
+    Provides a runtime query to find objects that are already instantiated
+    somewhere in your program. You could specify 'QLabel' and 'my_label', and
+    it will find the objects that belong to the class QLabel, and which
+    furthermore are referenced by a local variable or attribute
+    called 'my_label'. The results will include subclasses of QLabel too.
 
-    For example, if anywhere in your program you have said:
-
-        my_label = QLabel(), or
-        some_thing.my_label = QLabel()
-
-    You can find the QLabel object by specifying like this:
-
-        find_objects('my_label')
-
-    The search namespace is not as large as it seems on first sight. Please
-    refer to this class' constructor.
+    The search for objects of the qualifying classes is relatively expensive,
+    but is done at construction time only. The find_objects() query method then
+    has a relatively small search space is is inexpensive.
     """
 
     def __init__(self, class_filters):
         """
-        You are obliged to specify the classes of object the search should be
-        limited to. This helps performance by reducing the search
-        space, and allows the first 3 levels in a 5-deep nested loop to be run
-        just once - i.e. at construction time.
-        :param class_filters: A list of classes. E.g. (QWidget, QLayout)
+        :param class_filters: The list of classes (class objects) you wish
+        to search for instances of. E.g. (QWidget, QLabel).
         """
         if len(class_filters) == 0:
             raise RuntimeError('You must provide at least one class')
-        self._available_objects = self._assemble_available_objects(
-            class_filters)
+        self._available_objects = _assemble_available_objects(class_filters)
 
     def find_objects(self, reference_name):
         """
@@ -44,119 +35,146 @@ class ObjectFinder(object):
         :return: A list of objects that satisfy the search.
         """
         return [obj for obj in self._available_objects if
-                self._is_referenced_by_name(obj, reference_name)]
+                _is_referenced_by_name(obj, reference_name)]
 
-    #-------------------------------------------------------------------------
-    # Private below
-    #-------------------------------------------------------------------------
+        # -------------------------------------------------------------------------
+        # Private below
+        # -------------------------------------------------------------------------
 
-    def _assemble_available_objects(self, class_filters):
-        """
-        Provides a list of all objects known to the garbage collector that
-        belong to the classes cited. (Or their subclasses).
-        :param class_filters: The classes you wish to qualify.
-        (QWidget, QLayout).
-        :return: A sequence of objects.
-        """
-        all = self._get_all_objects()
-        filtered = [obj for obj in all if \
-                    self._belongs_to_one_of_these_classes(obj, class_filters)]
-        return filtered
 
-    def _get_all_objects(self):
-        """
-        Provides a sequence of all the objects being tracked by the garbage
-        collector at this time.
-        :return: The objects.
-        """
-        gc.collect() # Refreshes
-        first_tier_objs = gc.get_objects()# These are always container objects.
+def _filtered_on_class(objects, class_filters):
+    """
+    Returns a copy of the list of objects provided, from which have
+    been removed, all objects that do not belong to one of the classes
+    cited by the class filters. (Or their subclasses).
+    :param objects: Input objects.
+    :param class_filters: Qualifying classes.
+    :return: Output objects.
+    """
+    return [obj for obj in objects if
+            _belongs_to_one_of_these_classes(obj, class_filters)]
 
-        # Now add in the objects referred to inside the containers.
-        all_objects_by_id = {} # Avoid duplicates.
-        for first_tier_obj in first_tier_objs:
-            obj_id = id(first_tier_obj)
-            all_objects_by_id[obj_id] = first_tier_obj
-            self._add_second_tier_objects(first_tier_obj, all_objects_by_id)
-        return all_objects_by_id.values()
 
-    def _add_second_tier_objects(self, first_tier_obj, all_objects_by_id):
-        """
-        Augments the all_objects_by_id dictionary provided, with entries for
-        the objects referred to by the given first tier object.
-        :param first_tier_obj:
-        :param all_objects_by_id: The dictionary to update.
-        :return:
-        """
-        second_tier_objs = gc.get_referents(first_tier_obj)
-        for second_tier_obj in second_tier_objs:
-            obj_id = id(second_tier_obj)
-            all_objects_by_id[obj_id] = second_tier_obj
+def _is_referenced_by_name(obj, name):
+    """
+    Does a reference exist in the program that has the given name, and that
+    points to the given object of interest?
+    :param obj: The object of interest.
+    :param name: The name the reference must have.
+    :return: Boolean.
+    """
+    referrers = gc.get_referrers(obj)
 
-    def _filtered_on_class(self, objects, class_filters):
-        """
-        Returns a copy of the list of objects provided, from which have
-        been removed, all objects that do not belong to one of the classes
-        cited by the class filters. (Or their subclasses).
-        :param objects: Input objects.
-        :param class_filters: Qualifying classes.
-        :return: Output objects.
-        """
-        return [obj for obj in objects if
-                self._belongs_to_one_of_these_classes(obj, class_filters)]
+    # Look for a qualifying reference among referrers to the object that
+    # are stack frames. This will catch references that are local
+    # variables or function arguments.
+    if _is_referenced_by_a_stack_frame_name(referrers, obj, name):
+        return True
 
-    def _belongs_to_one_of_these_classes(self, obj, classes):
-        """
-        Returns true if the given object belongs to one of the classes cited
-        in the list of classes provided.
-        :param obj: The object to test.
-        :param classes: The qualifying classes.
-        :return: Boolean.
-        """
-        for cls in classes:
-            if isinstance(obj, cls):
-                return True
-        return False
+    # Look for a qualifying reference among referrers to the object that
+    # are dictionaries. This will catch references that are attributes
+    # of an instantiated class.
+    if _is_referenced_by_a_dict(referrers, obj, name):
+        return True
+    return False
 
-    def _is_referenced_by_name(self, obj, name):
-        """
-        Does a reference exist in the program that has the given name, and that
-        points to the given object of interest?
-        :param obj: The object of interest.
-        :param name: The name the reference must have.
-        :return: Boolean.
-        """
-        referrers = gc.get_referrers(obj)
 
-        # Is there a local variable called <name> that refers to the object?
-        # We answer by looking at referrers to the object that are of type
-        # frame.
-        if self._is_referenced_by_a_stack_frame_name(referrers, obj, name):
+def _assemble_available_objects(class_filters):
+    """
+    Provides a list of all objects known to the garbage collector that
+    belong to the classes cited. (Or their subclasses).
+    :param class_filters: The classes you wish to qualify.
+    (QWidget, QLayout).
+    :return: A sequence of objects.
+    """
+    all_objects = _get_all_objects()
+    filtered = [obj for obj in all_objects if
+                _belongs_to_one_of_these_classes(obj, class_filters)]
+    return filtered
+
+
+def _get_all_objects():
+    """
+    Provides a sequence of all the objects being tracked by the garbage
+    collector at this time.
+    :return: The objects.
+    """
+    gc.collect()  # Refresh
+    first_tier_objs = gc.get_objects()  # These are always container objects.
+
+    # Now add in the objects referred to inside the containers.
+    all_objects_by_id = {}  # Avoid duplicates.
+    for first_tier_obj in first_tier_objs:
+        obj_id = id(first_tier_obj)
+        all_objects_by_id[obj_id] = first_tier_obj
+        _add_second_tier_objects(first_tier_obj, all_objects_by_id)
+    return all_objects_by_id.values()
+
+
+def _add_second_tier_objects(first_tier_obj, all_objects_by_id):
+    """
+    Augments the all_objects_by_id dictionary provided, with entries for
+    the objects referred to by the given first tier object.
+    :param first_tier_obj:
+    :param all_objects_by_id: The dictionary to update.
+    :return:
+    """
+    second_tier_objs = gc.get_referents(first_tier_obj)
+    for second_tier_obj in second_tier_objs:
+        obj_id = id(second_tier_obj)
+        all_objects_by_id[obj_id] = second_tier_obj
+
+
+def _belongs_to_one_of_these_classes(obj, classes):
+    """
+    Returns true if the given object belongs to one of the classes cited
+    in the list of classes provided.
+    :param obj: The object to test.
+    :param classes: The qualifying classes.
+    :return: Boolean.
+    """
+    for cls in classes:
+        if isinstance(obj, cls):
             return True
-        return False
+    return False
 
-    def _is_referenced_by_a_stack_frame_name(self, referrers, obj, name):
-        """
-        Is there a reference among the given referrers, that is a stack frame,
-        which contains a local variable of the given name, which points to
-        the object of interest?
-        :param referrers: The references to scan.
-        :param obj: The object of interest.
-        :param name: The name the reference must have.
-        :return: Boolean.
-        """
-        frame_referrers = [ref for ref in referrers if self._is_a_frame(ref)]
-        for frame in frame_referrers:
-            if name in frame.f_locals:
-                object_referred_to = frame.f_locals[name]
-                if object_referred_to == obj:
-                    return True
-        return False
 
-    def _is_a_frame(self, obj):
-        """
-        Does the given object have the type 'frame'?
-        :param obj: The object of interest.
-        :return: Boolean.
-        """
-        return  obj.__class__.__name__ == 'frame'
+def _is_referenced_by_a_stack_frame_name(referrers, obj, name):
+    """
+    Is there a reference among the given referrers, that is a stack frame,
+    which contains a local variable of the given name, which points to
+    the object of interest?
+    :param referrers: The references to scan.
+    :param obj: The object of interest.
+    :param name: The name the reference must have.
+    :return: Boolean.
+    """
+    frame_referrers = [ref for ref in referrers if _is_a_(ref, 'frame')]
+    for frame in frame_referrers:
+        if name in frame.f_locals:
+            object_referred_to = frame.f_locals[name]
+            if object_referred_to == obj:
+                return True
+    return False
+
+
+def _is_referenced_by_a_dict(referrers, obj, name):
+    """
+    Is there a reference among the given referrers, that is a dict,
+    which contains a key which is the given name, which points to
+    the object of interest?
+    :param referrers: The references to scan.
+    :param obj: The object of interest.
+    :param name: The name the reference must have.
+    :return: Boolean.
+    """
+    dict_referrers = [ref for ref in referrers if _is_a_(ref, 'dict')]
+    for _dict in dict_referrers:
+        value = _dict.get(name, None)
+        if value == obj:
+            return True
+    return False
+
+
+def _is_a_(obj, class_name):
+    return obj.__class__.__name__ == class_name
--- src/qtlayoutbuilder/objectfinder_test.py
+++ src/qtlayoutbuilder/objectfinder_test.py
@@ -1,34 +1,54 @@
 from unittest import TestCase
 from PySide.QtGui import QLabel, QPushButton, QWidget, \
-    QLayout, qApp, QApplication
+    QLayout, qApp, QApplication, QHBoxLayout
 
 from objectfinder import ObjectFinder
 
 class OtherThing(object):
-    pass
+
+    def __init__(self):
+        self.my_layout = QHBoxLayout()
+        self.foo = 42
 
 class TestObjectFinder(TestCase):
 
     @classmethod
     def setUpClass(cls):
         """
-        We need to create (or reuse) a QApplication before we instantiate
-        any QWidget objects - or Qt emits warnings to the console.
+        This test needs to be run in the context of a QApplication.
         :return: None
         """
         super(TestObjectFinder, cls).setUpClass()
         if qApp is None:
             QApplication([])
 
-    def test_search_for_objects(self):
+    def test_search_for_objects_finding_local_variable(self):
+        # We create a local variable 'my_label' and then search for the object
+        # it is pointing to.
         my_label = QLabel()
-        my_button = QPushButton()
-        my_other_thing = OtherThing()
+        my_button = QPushButton() # just to add to the search space
         base_class_filters = [QWidget, QLayout]
         finder = ObjectFinder(base_class_filters)
-        found = finder.find_objects('my_label')
 
+        found = finder.find_objects('my_label')
         self.assertEquals(len(found), 1)
         found_object = found[0]
         self.assertEquals(found_object.__class__, QLabel)
 
+    def test_search_for_objects_finding_instance_attribute(self):
+        # We instantiate an 'OtherThing' and search for an object that is
+        # pointed to by the 'my_layout' attribute of that OtherThing.
+
+        # We are NOT searching for the 'other thing' itself, but for
+        # a QLayout object that 'other thing' refers to  with one of its
+        # attributes that is named 'my_layout. In other words we are searching
+        # for the the layout pointed to by other_thing.my_layout
+        other_thing = OtherThing()
+        my_button = QPushButton() # just to add to the search space
+        base_class_filters = [QWidget, QLayout]
+        finder = ObjectFinder(base_class_filters)
+
+        found = finder.find_objects('my_layout')
+        self.assertEquals(len(found), 1)
+        found_object = found[0]
+        self.assertEquals(found_object.__class__, QHBoxLayout)
