dea5ddb88012c876c7dd71371aaffe30ca3e7e38
peterhoward42 <peterhoward42@gmail.com>
2017-05-11 22:46:27 +0100

    Improving gui for input file devs.

--- src/qtlayoutbuilder/tools/builder_gui.py
+++ src/qtlayoutbuilder/tools/builder_gui.py
@@ -6,7 +6,6 @@ builds the layout and show()s the resultant top level widget.
 
 Plus a 'reformat' button that reformats the input file in-place.
 """
-
 from PySide.QtCore import QObject, QSettings
 from PySide.QtGui import qApp, QApplication, QFileDialog, QLayout
 
@@ -18,7 +17,6 @@ from qtlayoutbuilder.lib.multiline_string_utils import MultilineString
 _ORG = 'PARJI'
 _APP = 'QtLayoutBuilder'
 _LAST_KNOWN = '_lastknown'
-_DEFAULT_PATH = 'no_such_dir/no_such_file.txt'
 
 class BuilderGui(QObject):
     """
@@ -30,17 +28,24 @@ class BuilderGui(QObject):
         super(BuilderGui, self).__init__()
         self._last_user_widget_shown = None
         self._settings = QSettings(_ORG, _APP)
-
         self._input_path = self._last_known_input_file()
 
+        # Make this GUI
         self._layouts = self._make_gui()
 
-        self._layouts.at('path_label').setText(self._input_path)
+        # Augment this GUI
+        self._set_text_for_path_label()
+        self._layouts.at('build_btn').setToolTip(
+                'Run the builder and show() the layout created.')
+        self._layouts.at('format_btn').setToolTip(
+                'Reformat (and overwrite the input file')
 
+        # Connect signals.
         self._layouts.at('path_btn').clicked.connect(self._handle_path)
         self._layouts.at('build_btn').clicked.connect(self._handle_build)
         self._layouts.at('format_btn').clicked.connect(self._handle_reformat)
 
+        # And show it.
         self._layouts.at('main_page').show()
 
     def _make_gui(self):
@@ -48,30 +53,35 @@ class BuilderGui(QObject):
             main_page               QWidget
               page_layout           QVBoxLayout
                 path_controls       QHBoxLayout
+                  path_btn          QToolButton(Change Input File)
                   path_label        QLabel(Choose input file...)
-                  path_btn          QPushButton(Choose)
-                main_btns           QHBoxLayout
-                  build_btn         QPushButton(Build)
-                  format_btn        QPushButton(Reformat)
+                  build_btn         QToolButton(Build)
+                  format_btn        QToolButton(Reformat)
                 log_pane            QTextEdit
         """)
         return layouts
 
     def _handle_path(self):
-        path = QFileDialog.getOpenFileName(
-            None, 'Input file', self._input_path, 'Text files (*.txt)')
+        result = QFileDialog.getOpenFileName(self._layouts.at('main_page'),
+                'Choose input file', self._input_path)
+        try: # PySide 1.2.4 and who knows which other?
+            path, file_type_option_selected = result
+        except ValueError:
+            path = result # PyQt and mabye some PySide versions?
+        if not path:
+            return
         self._input_path = path
-        #self._settings.setValue(_LAST_KNOWN, self._input_path)
-        self._layouts.at('foo').setText(self._input_path)
+        self._settings.setValue(_LAST_KNOWN, self._input_path)
+        self._set_text_for_path_label()
 
     def _handle_build(self):
         try:
             users_layouts = build_from_file(
                 self._input_path, auto_format_and_overwrite=False)
         except LayoutError as e:
-            self._layouts.at('foo').setText(str(e))
+            self._layouts.at('log_pane').setText(str(e))
             return
-        top_item = users_layouts.get_first_top_level()
+        top_item = users_layouts.first_top_level_item()
         if isinstance(top_item, QLayout):
             self._users_layouts.at('foo').setText(
                 MultilineString.shift_left("""
@@ -80,33 +90,33 @@ class BuilderGui(QObject):
                     then I can.
                 """))
         else:
-            if self._last_user_widget_shown:
-                self._last_user_widget_shown.hide()
             self._last_user_widget_shown = top_item
             self._last_user_widget_shown.show()
-            self._users_layouts.at('foo').setText('Done')
+            self._layouts.at('log_pane').setText('Done')
 
     def _handle_reformat(self):
         try:
             users_layouts = build_from_file(
                 self._input_path, auto_format_and_overwrite=True)
-            self._users_layouts.at('foo').setText(
+            self._layouts.at('log_pane').setText(
                 MultilineString.shift_left("""
-                    Your input file has been reformatted and overwritten.
-                    You may need to reload it to see the changes in your
-                    editor.
+                    You may need to refresh your editor to see the
+                    formatting changes.
                 """))
         except LayoutError as e:
-            self._layouts.at('foo').setText(str(e))
+            self._layouts.at('log_pane').setText(
+                    "Cannot reformat the file because it won't build.")
             return
 
     def _last_known_input_file(self):
-        """
         last_known = self._settings.value(_LAST_KNOWN)
         if last_known:
             return last_known
-        """
-        return _DEFAULT_PATH # splittable default
+        return 'Please choose an input file'
+
+    def _set_text_for_path_label(self):
+        txt = '...' + self._input_path[-30:]
+        self._layouts.at('path_label').setText(txt)
 
 
 
