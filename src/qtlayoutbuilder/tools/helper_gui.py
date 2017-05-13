af2eb22e07c7c4099f5f872660883270d90def5b
peterhoward42 <peterhoward42@gmail.com>
2017-05-13 16:54:16 +0100

    wip

--- src/qtlayoutbuilder/tools/helper_gui.py
+++ src/qtlayoutbuilder/tools/helper_gui.py
@@ -1,15 +1,18 @@
 """
-A GUI program to help you be more productive while editing builder input files.
+A tool that aims to make it quicker and easier to iterate on developing your
+input files, by providing immediate feedback with no extra programming effort.
 
-It lets you specify an input file, and then offers a 'Build' button which
-builds the layout and show()s the resultant top level widget.
+It listens out for every change you make to your input file shows you 
+immediately what your GUI looks like.
 
-Plus a 'reformat' button that reformats the input file in-place.
+It also offers a REFORMAT button that reformats your input file in place.
+
+Usage: Just run helper_gui.py
 """
+import os
 
-from PySide.QtCore import QObject, QSettings
-from PySide.QtGui import qApp, QApplication, QFileDialog, QLayout, QWidget, \
-    QLabel
+from PySide.QtCore import QObject, QSettings, QTimer, QPoint
+from PySide.QtGui import qApp, QApplication, QFileDialog, QLayout, QWidget
 
 from qtlayoutbuilder.api.build import build_from_multi_line_string, \
     build_from_file
@@ -21,56 +24,51 @@ _APP = 'QtLayoutBuilder'
 _LAST_KNOWN = '_lastknown'
 
 class HelperGui(QObject):
-    """
-    Speeds up the process of editing and trying out a builder input file.
-    You specify your input file and then press a BUILD button any time you want.
-    
-    Also offers a FORMAT button that automatically reformats your input file,
-    and overwrites it (in place.)
-    
-    Usage: Just run helper_gui.py
-    """
 
     def __init__(self):
         super(HelperGui, self).__init__()
         self._settings = QSettings(_ORG, _APP)
         self._input_path = self._last_known_input_file()
+        self._last_shown_content = None
+        self._previous_timestamp = None
 
         # Make this GUI
         self._layouts = self._make_gui()
 
         # Augment this GUI
         self._layouts.at('main_page').setWindowTitle('Helper GUI')
-        self._layouts.at('build_btn').setToolTip(
-                'Run the builder and show() the layout created.')
         self._layouts.at('format_btn').setToolTip(
                 'Reformat (and overwrite the input file')
-
         self._set_text_for_path_label()
 
         # Connect signals.
         self._layouts.at('path_btn').clicked.connect(self._handle_path)
-        self._layouts.at('build_btn').clicked.connect(self._handle_build)
         self._layouts.at('format_btn').clicked.connect(self._handle_reformat)
 
         # And show it.
         self._layouts.at('main_page').show()
+
+        # Set up the timed call back to see if the input file has changed.
+        self._timer = QTimer()
+        self._timer.timeout.connect(self._timer_callback)
+        self._timer.start(1) # milli-seconds
 
     def _make_gui(self):
         layouts = build_from_multi_line_string("""
             main_page               QWidget
               page_layout           QVBoxLayout
                 path_controls       QHBoxLayout
-                  build_btn         QPushButton(Build)
                   path_label        QLabel(Choose input file...)
                   path_btn          QPushButton(Different File)
                   format_btn        QPushButton(Reformat)
-                  stretch           QSpacerItem
-                messages            QLabel(Messages show up here)
-                user_content        QGridLayout
+                log                 QLabel(Messages show up here)
                   
         """)
         return layouts
+
+
+    #-------------------------------------------------------------------------
+    # Event handlers
 
     def _handle_path(self):
         result = QFileDialog.getOpenFileName(self._layouts.at('main_page'),
@@ -86,12 +84,50 @@ class HelperGui(QObject):
         self._settings.setValue(_LAST_KNOWN, self._input_path)
         self._set_text_for_path_label()
 
-    def _handle_build(self):
+    def _handle_reformat(self):
         try:
             users_layouts = build_from_file(
-                self._input_path, auto_format_and_overwrite=False)
+                    self._input_path, auto_format_and_overwrite=True)
+            self._layouts.at('log').setText(
+                    MultilineString.shift_left("""
+                    Re format done! - You may need to refresh your editor to 
+                    see the formatting changes.
+                """))
         except LayoutError as e:
-            self._show_text_in_stack_widget(str(e))
+            self._layouts.at('log').setText(
+                    "Cannot reformat the file because it won't build.")
+            return
+
+    def _timer_callback(self):
+        if self._file_has_been_updated():
+            self._attempt_build()
+
+    #-------------------------------------------------------------------------
+    # Utility helpers
+
+    def _file_has_been_updated(self):
+        if not os.path.exists(self._input_path):
+            self._layouts.at('log').setText(
+                'Cannot check for file updates because the ' +
+                'specified input file does not exist.')
+            return False
+        new_time = self._get_mtime()
+        if new_time != self._previous_timestamp:
+            self._previous_timestamp = new_time
+            return True
+        return False
+
+    def _get_mtime(self):
+        # Protect against file appearing to be missing because an editor has
+        # locked it. Perhaps while saving it.
+        return os.path.getmtime(self._input_path)
+
+    def _attempt_build(self):
+        try:
+            users_layouts = build_from_file(self._input_path,
+                    auto_format_and_overwrite = False)
+        except LayoutError as e:
+            self._layouts.at('log').setText(str(e))
             return
         top_item = users_layouts.first_top_level_item()
         if isinstance(top_item, QWidget):
@@ -99,21 +135,8 @@ class HelperGui(QObject):
         if isinstance(top_item, QLayout):
             wrapper = QWidget(top_item)
             thing_to_show = wrapper
-        self._replace_users_layout(thing_to_show)
-
-    def _handle_reformat(self):
-        try:
-            users_layouts = build_from_file(
-                self._input_path, auto_format_and_overwrite=True)
-            self._show_text_in_stack_widget(
-                MultilineString.shift_left("""
-                    Done! - You may need to refresh your editor to see the
-                    formatting changes.
-                """))
-        except LayoutError as e:
-            self._show_text_in_stack_widget(
-                    "Cannot reformat the file because it won't build.")
-            return
+        self._show_built_content(thing_to_show)
+        self._layouts.at('log').setText('Build successful')
 
     def _last_known_input_file(self):
         last_known = self._settings.value(_LAST_KNOWN)
@@ -125,9 +148,14 @@ class HelperGui(QObject):
         txt = '<font color="grey">...%s</font>' % self._input_path[-30:]
         self._layouts.at('path_label').setText(txt)
 
-    def _replace_users_layout(self, thing_to_show):
-        grid = self._layouts.at('user_content')
-        grid.addWidget(thing_to_show, 0, 0)
+    def _show_built_content(self, thing_to_show):
+        if self._last_shown_content is not None:
+            self._last_shown_content.hide()
+        self._last_shown_content = thing_to_show
+        this_gui = self._layouts.at('main_page')
+        show_at = QPoint(0,0)
+        thing_to_show.move(show_at)
+        self._last_shown_content.show()
 
 if __name__ == '__main__':
     QApplication([])
