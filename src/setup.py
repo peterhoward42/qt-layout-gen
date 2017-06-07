459fc022546c710b2231116b1bfbc2c4353eeed6
peterhoward42 <peterhoward42@gmail.com>
2017-06-07 21:15:09 +0100

    First attempt to specify a setup.py for uploading to PyPi.

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'qtlayoutbuilder',
+  packages = ['qtlayoutbuilder'],
+  version = '0.1',
+  description = 'A library to make PySide layouts quickly and easily',
+  author = 'Peter Howard',
+  author_email = 'peterhoward42@gmail.com',
+  url = 'https://github.com/peterhoward42/qtlayoutbuilder',
+  download_url = 'https://github.com/peterhoward42/qtlayoutbuilder/archive/0.1.tar.gz',
+  keywords = ['PySide', 'Layouts'],
+  classifiers = [],
+)
